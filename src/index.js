11ba61d73aba9f137329831d11cfd96cb88339b7
felipe <riopipe8@gmail.com>
2022-11-24 15:43:30 -0300

    finish main page style css

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const content = document.getElementById("content");
+const header = document.createElement("div");
+    const logo = document.createElement("div");
+    const nav = document.createElement("nav");
+const main = document.createElement("div");
+    const hero = document.createElement("h1");
+    const button = document.createElement("button");
